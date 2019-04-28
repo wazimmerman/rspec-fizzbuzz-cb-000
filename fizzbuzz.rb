a5a02c93e6726475705b999d3569e14578a0e74e
William Zimmerman <william.a.zimmerman@gmail.com>
2019-04-28 02:37:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
-  elsif int % 5
+  elsif int % 5 == 0
     return "Buzz"
-  elsif int % 15
+  elsif int % 15 == 0
     return "FizzBuzz"
 end
 end
